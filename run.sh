54668549c78f47b1e6973ebdf63992475269ac1c
zjp <jiping_zhou@foxmail.com>
2025-08-13 07:04:14 +0000

    run.sh: add merge_results to generate merge_results-core.json

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@
 set -eou pipefail
 
 merge_diff() {
-  jq 'map(select(.proof_kind != null)) | sort_by(.file, .func)' merge_diff.json >merge_diff-proofs-only.json
+  jq '
+    map(select(.proof_kind != null) | .harness = .func | del(.func))
+    | sort_by(.file, .harness)
+  ' merge_diff.json >merge_diff-proofs-only.json
 }
+
 results() {
   jq '[
     .[] | select(.result.crate == "core") # filter in core results
@@ -15,7 +19,7 @@ results() {
     | . += (if (.result // "" | startswith("SUCCESSFUL") | not) then {ok: false} else {} end) # convert SUCCESSFUL to ok, omission as true
     | .props = .n_total_properties | del(.n_total_properties) # shorten long field names
     | .file = .file_name | del(.file_name) # shorten long field names
-    | .func = { name: .function, safe: .function_safeness } | del(.function, .function_safeness) # shorten long field names
+    | .func_ = { name: .function, safe: .function_safeness } | del(.function, .function_safeness) # shorten long field names
     | del(.time | select(. == null))      # remove null time
     | del(.output | select(. == []))      # remove .output field if empty
     | del(.crate, .result, .public_target, .autoharness_result)  # remove less important fields to save space
@@ -23,9 +27,21 @@ results() {
   ]' ../../tmp/ubuntu-latest-results.json/results.json >results-core.json
 }
 
+merge_results() {
+  jq --slurp '
+    (.[0] + .[1])
+    | group_by(.harness + "#" + .file)
+    | map(
+        add | { file, harness , proof_kind, time, props, with_contract, func, hash }
+      )
+    | walk(if type == "object" then with_entries(select(.value != null)) else . end)
+  ' results-core.json merge_diff-proofs-only.json >merge_results-core.json
+}
+
 declare -A cmds=(
   [merge_diff]=merge_diff
   [results]=results
+  [merge_results]=merge_results
 )
 
 [[ $# -eq 0 ]] && {
