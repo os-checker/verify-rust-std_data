818af9f5efc8867c6b493f097362967db3b437d6
zjp <jiping_zhou@foxmail.com>
2025-10-29 12:28:24 +0000

    ci: dv-mnt

---
+++ .github/mnt.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+need_k=$((50 * 1024 * 1024))
+avail_k=$(df -k /mnt | awk 'NR==2{print $4}')
+
+df -alh
+
+if ((avail_k < need_k)); then
+  git submodule update --init --recursive kani verify-rust-std
+  exit 0
+fi
+
+echo "Mount /mnt (/dev/sda1) to verify-rust-std because the available space is $((avail_k / 1024 / 1024))G"
+
+TARGET_DIR=$PWD/verify-rust-std
+
+s -lhd .
+rm "$TARGET_DIR" -rf
+mkdir "$TARGET_DIR" -p
+ls -lhd .
+
+sudo mkdir -p /mnt/my
+sudo chown runner:runner /mnt/my
+sudo chmod 755 /mnt/my
+
+rsync -aHAX "$TARGET_DIR"/ /mnt/my/
+sudo mount --bind /mnt/my "$TARGET_DIR"
+
+git submodule update --init --recursive kani verify-rust-std
+
+ls -alh "$TARGET_DIR"
+ls -alh /mnt/my
+
+df -alh
